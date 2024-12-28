288fd6c6252cd1db371c534818255de2866804a7
thomas.zh <thomas.zh@qq.com>
2024-12-28 21:43:14 +0800

    feat: add formDefine & dataview API

---
+++ apps/developer/src/hooks/api-dataview.js
@@ -0,0 +1,111 @@
+import { SERVICE_HOST_POST as host } from '@/config';
+import { vanillaPostData, vanillaDeleteData, vanillaPutData, vanillaGetData } from '.';
+
+/**
+ * 查询数据视图定义列表
+ * @returns {Promise} form list
+ */
+export const fetchDataviewList = async () => {
+  const params = {
+    currPage: 1,
+    pageSize: 100,
+    orders: [
+      {
+        column: sequence,
+        dir: 'asc',
+      },
+    ],
+    searchs: [],
+  };
+  const result = await vanillaPostData(
+    `${host}/api/formas/dataviews/filter`,
+    params
+  );
+  return result;
+};
+
+
+/**
+ * 创建数据视图定义
+ * @param {{moduleId: string, formDefineId: string, title: string, note: string, sequence:string}} item
+ */
+export const createDataview = async item => {
+  const { moduleId, title, note, sequence } = item;
+  const result = await vanillaPostData(`${host}/api/formas/dataviews`, {
+    moduleId: moduleId,
+    formDefineId: formDefineId,
+    title: title,
+    note: note,
+    sequence: sequence,
+  });
+  return result;
+};
+
+
+/**
+ * 修改数据视图定义
+ */
+export const updateDataview = async item => {
+  const result = await vanillaPutData(
+    `${host}/api/formas/dataviews/${item.key}`,
+    {
+      title: item.title,
+      note: item.note,
+      sequence: item.sequence,
+    }
+  );
+  return result;
+};
+
+
+/**
+ * 删除数据视图定义
+ * @param {string} key dataview id
+ */
+export const removeDataview = async key => {
+  const result = await vanillaDeleteData(`${host}/api/formas/dataviews/${key}`);
+  return result;
+};
+
+
+/**
+ * 修改数据视图定义状态
+ */
+export const updateDataviewStatus = async item => {
+  const result = await vanillaPutData(
+    `${host}/api/formas/dataviews/${item.key}/status`,
+    {
+      status: item.status,
+    }
+  );
+  return result;
+};
+
+
+/**
+ * 修改数据视图定义模块
+ */
+export const updateDataviewModule = async item => {
+  const result = await vanillaPutData(
+    `${host}/api/formas/dataviews/${item.key}/module`,
+    {
+      moduleId: item.moduleId,
+    }
+  );
+  return result;
+};
+
+
+/**
+ * 修改数据视图定义SQL配置信息
+ */
+export const updateDataviewSql = async item => {
+  const result = await vanillaPutData(
+    `${host}/api/formas/dataviews/${item.key}/sql`,
+    {
+      sqlStmt: item.sqlStmt,
+      sqlParams: item.sqlParams,
+    }
+  );
+  return result;
+};
---
+++ apps/developer/src/hooks/api-form.js
@@ -0,0 +1,109 @@
+import { SERVICE_HOST_POST as host } from '@/config';
+import { vanillaPostData, vanillaDeleteData, vanillaPutData, vanillaGetData } from '.';
+
+/**
+ * 查询表单定义列表
+ * @returns {Promise} form list
+ */
+export const fetchFormDefineList = async () => {
+  const params = {
+    currPage: 1,
+    pageSize: 100,
+    orders: [
+      {
+        column: sequence,
+        dir: 'asc',
+      },
+    ],
+    searchs: [],
+  };
+  const result = await vanillaPostData(
+    `${host}/api/formas/form_defines/filter`,
+    params
+  );
+  return result;
+};
+
+
+/**
+ * 创建表单定义
+ * @param {{moduleId: string, title: string, note: string, sequence:string}} item
+ */
+export const createFormDefine = async item => {
+  const { moduleId, title, note, sequence } = item;
+  const result = await vanillaPostData(`${host}/api/formas/form_defines`, {
+    moduleId: moduleId,
+    title: title,
+    note: note,
+    sequence: sequence,
+  });
+  return result;
+};
+
+
+/**
+ * 修改表单定义
+ */
+export const updateFormDefine = async item => {
+  const result = await vanillaPutData(
+    `${host}/api/formas/form_defines/${item.key}`,
+    {
+      title: item.title,
+      note: item.note,
+      sequence: item.sequence,
+    }
+  );
+  return result;
+};
+
+
+/**
+ * 删除表单定义
+ * @param {string} key formDefine id
+ */
+export const removeFormDefine = async key => {
+  const result = await vanillaDeleteData(`${host}/api/formas/form_defines/${key}`);
+  return result;
+};
+
+
+/**
+ * 修改表单定义状态
+ */
+export const updateFormDefineStatus = async item => {
+  const result = await vanillaPutData(
+    `${host}/api/formas/form_defines/${item.key}/status`,
+    {
+      status: item.status,
+    }
+  );
+  return result;
+};
+
+
+/**
+ * 修改表单定义模块
+ */
+export const updateFormDefineModule = async item => {
+  const result = await vanillaPutData(
+    `${host}/api/formas/form_defines/${item.key}/module`,
+    {
+      moduleId: item.moduleId,
+    }
+  );
+  return result;
+};
+
+
+/**
+ * 修改表单定义配置信息
+ */
+export const updateFormDefineSchema = async item => {
+  const result = await vanillaPutData(
+    `${host}/api/formas/form_defines/${item.key}/schema`,
+    {
+      schema: item.schema,
+    }
+  );
+  return result;
+};
